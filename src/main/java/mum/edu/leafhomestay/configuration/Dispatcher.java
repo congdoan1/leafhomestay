413ded0a4a011a9c32bf2b5dde5929a641e18a96
Cong Toan Doan <toandc1152@gmail.com>
2019-11-17 20:27:27 -0600

    Convert to java config

---
+++ src/main/java/mum/edu/leafhomestay/configuration/Dispatcher.java
@@ -0,0 +1,70 @@
+package mum.edu.leafhomestay.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import mum.edu.leafhomestay.interceptor.NoticeInterceptor;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "mum.edu.leafhomestay.controller" })
+@PropertySource(value = "classpath:application.properties")
+public class Dispatcher extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	Environment environment;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(environment.getProperty("staticResourceAlias"))
+				.addResourceLocations(environment.getProperty("staticResourceLocation"));
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix(environment.getProperty("viewResolver.prefix"));
+		bean.setSuffix(environment.getProperty("viewResolver.suffix"));
+		return bean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new NoticeInterceptor()).addPathPatterns("/employees/list");
+	}
+
+	@Bean(name = "messageSource")
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
+		resource.setBasenames("classpath:messages", "classpath:errorMessages");
+		resource.setDefaultEncoding("UTF-8");
+		return resource;
+	}
+
+	@Bean(name = "validator")
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+
+	@Override
+	public Validator getValidator() {
+		return validator();
+	}
+
+}
---
+++ src/main/java/mum/edu/leafhomestay/configuration/Service.java
@@ -0,0 +1,10 @@
+package mum.edu.leafhomestay.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("mum.edu.leafhomestay.service.impl")
+public class Service {
+
+}
